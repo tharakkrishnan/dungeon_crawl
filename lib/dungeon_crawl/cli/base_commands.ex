4d2881863b2041915e3d37401422d4ce508305f3
Tharak Krishnan <tharak.krishnan@gmail.com>
2018-03-17 21:20:06 -0700

    add eexception handling

--- lib/dungeon_crawl/cli/base_commands.ex
+++ lib/dungeon_crawl/cli/base_commands.ex
@@ -1,11 +1,42 @@
 defmodule DungeonCrawl.CLI.BaseCommands do
     alias Mix.Shell.IO, as: Shell
   
+    def ask_for_option(options) do
+      index = ask_for_index(options)
+      chosen_option = Enum.at(options, index)
+      chosen_option
+        || (display_invalid_option() && ask_for_option(options))
+    end
+  
+    def ask_for_index(options) do
+      answer =
+        options
+        |> display_options
+        |> generate_question
+        |> Shell.prompt
+        |> Integer.parse
+  
+      case answer do
+        :error ->
+          display_invalid_option()
+          ask_for_index(options)
+        {option, _} ->
+          option - 1
+      end
+    end
+  
+    def display_invalid_option do
+      Shell.cmd("clear")
+      Shell.error("Invalid option.")
+      Shell.prompt("Press Enter to try again.")
+      Shell.cmd("clear")
+    end
+  
     def display_options(options) do
       options
       |> Enum.with_index(1)
       |> Enum.each(fn {option, index} ->
-        Shell.info("#{index} - #{option}")
+        Shell.info("#{index} - #{DungeonCrawl.Display.info(option)}")
       end)
   
       options
@@ -14,10 +45,5 @@ defmodule DungeonCrawl.CLI.BaseCommands do
     def generate_question(options) do
       options = Enum.join(1..Enum.count(options),",")
       "Which one? [#{options}]\n"
-    end
-  
-    def parse_answer(answer) do
-      {option, _} = Integer.parse(answer)
-      option - 1
     end
   end
--- lib/dungeon_crawl/cli/room_actions_choice.ex
+++ lib/dungeon_crawl/cli/room_actions_choice.ex
@@ -2,20 +2,9 @@ defmodule DungeonCrawl.CLI.RoomActionsChoice do
     alias Mix.Shell.IO, as: Shell
     import DungeonCrawl.CLI.BaseCommands
   
-    def start(room) do
-      room_actions = room.actions
-      find_action_by_index = &(Enum.at(room_actions, &1))
-  
+    def start(room) do  
       Shell.info(room.description())
-  
-      chosen_action =
-        room_actions
-        |> display_options
-        |> generate_question
-        |> Shell.prompt
-        |> parse_answer
-        |> find_action_by_index.()
-  
+      chosen_action =ask_for_option(room.actions)
       {room, chosen_action}
     end
   end
